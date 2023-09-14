391ddea8bb26cd087bd720d0c78fc46ff1b7e8a4
James Alhassan <jamesalhassan052@gmail.com>
2023-09-14 15:46:10 +0000

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route index element={<Home />} />
           <Route index element={<Home />} />
         </Route>
       </Routes>
