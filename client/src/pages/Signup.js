f47e77d5b6edca625c887ddd1dc6e62926b84cd2
James Alhassan <jamesalhassan052@gmail.com>
2023-09-17 15:45:45 +0000

    Update signup css

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,63 +1,135 @@
+import { useState } from "react";
 const Signup = () => {
+  const [docSignup, setDocSignup] = useState(false);
+  const handleDocSignup = () => {
+    setDocSignup((current) => !current);
+  };
   return (
     <div className='form'>
-      <form>
-        <div class='form_wrap'>
-          <div class='input_grp'>
-            <div class='input_wrap'>
-              <label for='fname'>First Name</label>
-              <input type='text' id='fname' required />
+      <button className='btn' onClick={handleDocSignup}>
+        {docSignup ? "signup as a patient" : "signup as a doctor"}
+      </button>
+      {docSignup ? (
+        <form>
+          <div class='form_wrap'>
+            <div class='input_grp'>
+              <div class='input_wrap'>
+                <label for='fname'>First Name</label>
+                <input type='text' id='fname' required />
+              </div>
+              <div class='input_wrap'>
+                <label for='lname'>Last Name</label>
+                <input type='text' id='lname' required />
+              </div>
             </div>
             <div class='input_wrap'>
-              <label for='lname'>Last Name</label>
-              <input type='text' id='lname' required />
+              <label for='email'>Email Address</label>
+              <input type='text' id='email' required />
+            </div>
+            <div class='input_wrap'>
+              <label>Gender</label>
+              <ul>
+                <li>
+                  <label class='radio_wrap'>
+                    <input
+                      type='radio'
+                      name='gender'
+                      value='male'
+                      class='input_radio'
+                      checked
+                    />
+                    <span>Male</span>
+                  </label>
+                </li>
+                <li>
+                  <label class='radio_wrap'>
+                    <input
+                      type='radio'
+                      name='gender'
+                      value='female'
+                      class='input_radio'
+                    />
+                    <span>Female</span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+            <div class='input_wrap'>
+              <label for='dob'>DOB</label>
+              <input type='date' id='dob' required />
+            </div>
+            <div class='input_wrap'>
+              <label for='hospital'>hospital</label>
+              <input type='text' id='hospital' />
+            </div>
+            <div class='input_wrap'>
+              <label for='license'>license #</label>
+              <input type='text' id='license' />
+            </div>
+            <div class='input_wrap'>
+              <input type='submit' value='Register Now' class='submit_btn' />
             </div>
           </div>
-          <div class='input_wrap'>
-            <label for='email'>Email Address</label>
-            <input type='text' id='email' required />
+        </form>
+      ) : (
+        <form>
+          <div class='form_wrap'>
+            <div class='input_grp'>
+              <div class='input_wrap'>
+                <label for='fname'>First Name</label>
+                <input type='text' id='fname' required />
+              </div>
+              <div class='input_wrap'>
+                <label for='lname'>Last Name</label>
+                <input type='text' id='lname' required />
+              </div>
+            </div>
+            <div class='input_wrap'>
+              <label for='email'>Email Address</label>
+              <input type='text' id='email' required />
+            </div>
+            <div class='input_wrap'>
+              <label>Gender</label>
+              <ul>
+                <li>
+                  <label class='radio_wrap'>
+                    <input
+                      type='radio'
+                      name='gender'
+                      value='male'
+                      class='input_radio'
+                      checked
+                    />
+                    <span>Male</span>
+                  </label>
+                </li>
+                <li>
+                  <label class='radio_wrap'>
+                    <input
+                      type='radio'
+                      name='gender'
+                      value='female'
+                      class='input_radio'
+                    />
+                    <span>Female</span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+            <div class='input_wrap'>
+              <label for='dob'>DOB</label>
+              <input type='date' id='dob' required />
+            </div>
+            <div class='input_wrap'>
+              <label for='other'>Other</label>
+              <input type='text' id='other' />
+            </div>
+            <div class='input_wrap'>
+              <input type='submit' value='Register Now' class='submit_btn' />
+            </div>
           </div>
-          <div class='input_wrap'>
-            <label>Gender</label>
-            <ul>
-              <li>
-                <label class='radio_wrap'>
-                  <input
-                    type='radio'
-                    name='gender'
-                    value='male'
-                    class='input_radio'
-                    checked
-                  />
-                  <span>Male</span>
-                </label>
-              </li>
-              <li>
-                <label class='radio_wrap'>
-                  <input
-                    type='radio'
-                    name='gender'
-                    value='female'
-                    class='input_radio'
-                  />
-                  <span>Female</span>
-                </label>
-              </li>
-            </ul>
-          </div>
-          <div class='input_wrap'>
-            <label for='dob'>DOB</label>
-            <input type='date' id='dob' required />
-          </div>
-          <div class='input_wrap'>
-            <label for='other'>Other</label>
-            <input type='text' id='other' />
-          </div>
-          <div class='input_wrap'>
-            <input type='submit' value='Register Now' class='submit_btn' />
-          </div>
-        </div>
-      </form>
+        </form>
+      )}
     </div>
   );
 };
