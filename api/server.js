5a53fe5c5cb230713060b1732a0cb7a128720677
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-18 09:27:52 +0000

    Add cors and cookie after successful login

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // extra packages
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // connect DB
 const mongoose = require('mongoose');
