3ee8b0f60b4202cb8bc14575aa7bd85ab57ce021
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-21 03:11:28 +0000

    Included and implement @tanstack library to query data

--- client/src/pages/allDoctors/AllDoctors.js
+++ client/src/pages/allDoctors/AllDoctors.js
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import DoctorCard from "../../components/doctorCard/DoctorCard";
 import "./AllDoctors.css";
+import newRequest from "../../utils/newRequest";
 
 const AllDoctors = () => {
+
+    const { isLoading, error, data, refetch } = useQuery({
+        queryKey:[],
+        queryFn: () =>
+            newRequest.get(
+                '/doctor'
+            ).then((res) => {
+                console.log(res.data)
+                return res.data;
+            }),
+    });
+
+    // when page is loading
+    if (isLoading) return <div>Loading</div>;
+
+    // when there is an error
+    if (error) return <div>An eror has occured</div>
+
     return (
         <div className="allDoctors">
+        
             <ul>
                 <li><DoctorCard /></li>
                 <li><DoctorCard /></li>
