b38ca175af2252f1cc4a961c91accdd791f11164
James Alhassan <jamesalhassan052@gmail.com>
2023-09-24 19:19:09 +0000

    Update profile.js

--- client/src/pages/Dashboard/Profile.js
+++ client/src/pages/Dashboard/Profile.js
@@ -1,117 +1,89 @@
 const Profile = () => {
   return (
-    <main class='has-dflex-center'>
-      <section>
-        <div class='lx-container-70'>
-          <div class='lx-row'>
-            <h1 class='title'>Edit your profile</h1>
-          </div>
-          <div class='lx-row align-stretch'>
-            <div class='lx-column column-user-pic'>
-              <div class='profile-pic bs-md'>
-                <h1 class='pic-label'>Profile picture</h1>
-                <div class='pic bs-md'>
-                  <img
-                    src='https://bit.ly/3jRbrbp'
-                    alt=''
-                    width='4024'
-                    height='6048'
-                    loading='lazy'
-                  />
-                  <a id='change-avatar' class='lx-btn'>
-                    <i class='fas fa-camera-retro'></i>&nbsp;&nbsp;Change your
-                    profile picture.
-                  </a>
-                </div>
-                <div class='pic-info'>
-                  <p>
-                    <i class='fas fa-exclamation-triangle'></i>&nbsp;&nbsp;This
-                    photo will appear on the platform, in your contributions or
-                    where it is mentioned.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class='lx-column'>
-              <form action='get'>
-                <div class='fieldset'>
-                  <label for='user-name'>Name</label>
-                  <div class='input-wrapper'>
-                    <span class='icon'>
-                      <i class='fas fa-user'></i>
-                    </span>
-                    <input
-                      type='text'
-                      id='user-name'
-                      value='Lorem Ipsum'
-                      autocomplete='username'
-                      required
-                    />
-                  </div>
-                  <div id='user-name-helper' class='helper'>
-                    <p>
-                      Your name can appear on the platform, in your
-                      contributions or where it is mentioned.
-                    </p>
-                  </div>
-                </div>
-                <div class='fieldset'>
-                  <label for='user-id'>Registration</label>
-                  <div class='input-wrapper'>
-                    <span class='icon'>
-                      <i class='fas fa-address-card'></i>
-                    </span>
-                    <input type='number' id='user-id' value='424242' required />
-                  </div>
-                  <div id='user-id-helper' class='helper'></div>
-                </div>
-                <div class='fieldset'>
-                  <label for='email'>E-mail</label>
-                  <div class='input-wrapper'>
-                    <span class='icon'>
-                      <i class='fas fa-envelope'></i>
-                    </span>
-                    <input
-                      type='email'
-                      id='email'
-                      value='lorem@ipsum.com'
-                      autocomplete='username'
-                    />
-                  </div>
-                  <div id='email-helper' class='helper'></div>
-                </div>
-                <div class='fieldset'>
-                  <label for='pass'>Password</label>
-                  <div class='input-wrapper'>
-                    <span class='icon'>
-                      <i class='fas fa-key'></i>
-                    </span>
-                    <input
-                      type='password'
-                      id='pass'
-                      value='pass123*'
-                      autocomplete='current-password'
-                    />
-                  </div>
-                  <div id='pass-helper' class='helper'></div>
-                </div>
-                <div class='actions'>
-                  <a id='cancel' class='lx-btn'>
-                    <i class='fas fa-ban'></i>&nbsp;&nbsp;Cancel
-                  </a>
-                  <a id='clear' class='lx-btn'>
-                    <i class='fas fa-broom'></i>&nbsp;&nbsp;Clean
-                  </a>
-                  <a id='save' class='lx-btn'>
-                    <i class='fas fa-save'></i>&nbsp;&nbsp;Save
-                  </a>
-                </div>
-              </form>
-            </div>
+    <div class='container'>
+      <h1>Edit Profile</h1>
+      <hr />
+      <div class='row'>
+        {/* <!-- left column --> */}
+        <div class='col-md-3'>
+          <div class='text-center'>
+            <img
+              src='https://png.pngitem.com/pimgs/s/150-1503945_transparent-user-png-default-user-image-png-png.png'
+              class='avatar img-circle'
+              alt='avatar'
+            />
+            <h6>Upload a different photo...</h6>
+
+            <input type='file' class='form-control' />
           </div>
         </div>
-      </section>
-    </main>
+
+        {/* <!-- edit form column --> */}
+        <div class='col-md-9 personal-info'>
+          <div class='alert alert-info alert-dismissable'>
+            <a class='panel-close close' data-dismiss='alert'>
+              ×
+            </a>
+            <i class='fa fa-coffee'></i>
+            This is an <strong>.alert</strong>. Use this to show important
+            messages to the user.
+          </div>
+          <h3>Personal info</h3>
+
+          <form class='form-horizontal' role='form'>
+            <div class='form-group'>
+              <label class='col-md-3 control-label'>Username:</label>
+              <div class='col-md-8'>
+                <input class='form-control' type='text' value='janeuser' />
+              </div>
+            </div>
+            <div class='form-group'>
+              <label class='col-lg-3 control-label'>Email:</label>
+              <div class='col-lg-8'>
+                <input
+                  class='form-control'
+                  type='text'
+                  value='janesemail@gmail.com'
+                />
+              </div>
+            </div>
+
+            <div class='form-group'>
+              <label class='col-md-3 control-label'>Password:</label>
+              <div class='col-md-8'>
+                <input
+                  class='form-control'
+                  type='password'
+                  value='11111122333'
+                />
+              </div>
+            </div>
+            <div class='form-group'>
+              <label class='col-md-3 control-label'>Confirm password:</label>
+              <div class='col-md-8'>
+                <input
+                  class='form-control'
+                  type='password'
+                  value='11111122333'
+                />
+              </div>
+            </div>
+            <div class='form-group'>
+              <label class='col-md-3 control-label'></label>
+              <div class='col-md-8'>
+                <input
+                  type='button'
+                  class='btn btn-primary'
+                  value='Save Changes'
+                />
+                <span></span>
+                <input type='reset' class='btn btn-default' value='Cancel' />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 export default Profile;
