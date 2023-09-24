f836eff4f932fc0d5c31ac2832fd5922b0ec17cf
James Alhassan <jamesalhassan052@gmail.com>
2023-09-24 21:18:53 +0000

    Add css

--- client/src/pages/Dashboard/Profile.js
+++ client/src/pages/Dashboard/Profile.js
@@ -1,19 +1,76 @@
 import proPic from "../../assets/proPic.png";
 const Profile = () => {
   return (
-    <>
-      <div id='root'></div>
-      <a
-        target='_blank'
-        title='instagram/web__addict'
-        href='https://www.instagram.com/web__addict/'
-      >
-        <i class='fab fa-instagram'></i>
-      </a>
-      <svg viewBox='0 0 512 512' width='100' title='arrow-alt-circle-right'>
-        <path d='M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z' />
-      </svg>
-    </>
+    <div>
+      <div class='wrapper'>
+        <div class='pro'>
+          <div class='content'>
+            <h1>Edit Profile</h1>
+            <form action=''>
+              {/* <!-- Photo --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='avatar'>Your Photo</label>
+                </div>
+                <div class='grid-65'>
+                  <span class='photo' title='Upload your Avatar!'></span>
+                  <input type='file' class='btn' />
+                </div>
+              </fieldset>
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='fname'>First Name</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='fname' tabindex='1' />
+                </div>
+              </fieldset>
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='lname'>Last Name</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='lname' tabindex='2' />
+                </div>
+              </fieldset>
+
+              {/* <!-- Location --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='location'>Location</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='location' tabindex='4' />
+                </div>
+              </fieldset>
+              {/* <!-- Country --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='country'>Country</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='country' tabindex='5' />
+                </div>
+              </fieldset>
+              {/* <!-- Email --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='email'>Email Address</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='email' id='email' tabindex='6' />
+                </div>
+              </fieldset>
+
+              <fieldset>
+                <input type='button' class='Btn cancel' value='Cancel' />
+                <input type='submit' class='Btn' value='Save Changes' />
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default Profile;
