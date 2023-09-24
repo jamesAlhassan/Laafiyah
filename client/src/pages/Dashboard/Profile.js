29ce6a0c536359243a51d798c411bcfc95e7c566
James Alhassan <jamesalhassan052@gmail.com>
2023-09-24 19:37:50 +0000

    Update

--- client/src/pages/Dashboard/Profile.js
+++ client/src/pages/Dashboard/Profile.js
@@ -1,89 +1,19 @@
+import proPic from "../../assets/proPic.png";
 const Profile = () => {
   return (
-    <div class='container'>
-      <h1>Edit Profile</h1>
-      <hr />
-      <div class='row'>
-        {/* <!-- left column --> */}
-        <div class='col-md-3'>
-          <div class='text-center'>
-            <img
-              src='https://png.pngitem.com/pimgs/s/150-1503945_transparent-user-png-default-user-image-png-png.png'
-              class='avatar img-circle'
-              alt='avatar'
-            />
-            <h6>Upload a different photo...</h6>
-
-            <input type='file' class='form-control' />
-          </div>
-        </div>
-
-        {/* <!-- edit form column --> */}
-        <div class='col-md-9 personal-info'>
-          <div class='alert alert-info alert-dismissable'>
-            <a class='panel-close close' data-dismiss='alert'>
-              ×
-            </a>
-            <i class='fa fa-coffee'></i>
-            This is an <strong>.alert</strong>. Use this to show important
-            messages to the user.
-          </div>
-          <h3>Personal info</h3>
-
-          <form class='form-horizontal' role='form'>
-            <div class='form-group'>
-              <label class='col-md-3 control-label'>Username:</label>
-              <div class='col-md-8'>
-                <input class='form-control' type='text' value='janeuser' />
-              </div>
-            </div>
-            <div class='form-group'>
-              <label class='col-lg-3 control-label'>Email:</label>
-              <div class='col-lg-8'>
-                <input
-                  class='form-control'
-                  type='text'
-                  value='janesemail@gmail.com'
-                />
-              </div>
-            </div>
-
-            <div class='form-group'>
-              <label class='col-md-3 control-label'>Password:</label>
-              <div class='col-md-8'>
-                <input
-                  class='form-control'
-                  type='password'
-                  value='11111122333'
-                />
-              </div>
-            </div>
-            <div class='form-group'>
-              <label class='col-md-3 control-label'>Confirm password:</label>
-              <div class='col-md-8'>
-                <input
-                  class='form-control'
-                  type='password'
-                  value='11111122333'
-                />
-              </div>
-            </div>
-            <div class='form-group'>
-              <label class='col-md-3 control-label'></label>
-              <div class='col-md-8'>
-                <input
-                  type='button'
-                  class='btn btn-primary'
-                  value='Save Changes'
-                />
-                <span></span>
-                <input type='reset' class='btn btn-default' value='Cancel' />
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <>
+      <div id='root'></div>
+      <a
+        target='_blank'
+        title='instagram/web__addict'
+        href='https://www.instagram.com/web__addict/'
+      >
+        <i class='fab fa-instagram'></i>
+      </a>
+      <svg viewBox='0 0 512 512' width='100' title='arrow-alt-circle-right'>
+        <path d='M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z' />
+      </svg>
+    </>
   );
 };
 export default Profile;
