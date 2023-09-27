777b8dbca9ca2a22202c5f0939aff96da585bdd4
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-27 20:14:16 +0000

    Work from work to continue at home

--- client/src/pages/doctorDashboard/AppointmentList.js
+++ client/src/pages/doctorDashboard/AppointmentList.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './AppointmentList.css';
+import newRequest from '../../utils/newRequest';
 
-function AppointmentList({ appointments }) {
+function AppointmentList({ doctorId }) {
+
+  const [appointments, setAppointments] = useState({});
+  const [isEmpty, setIsEmpty] = useState(false);
+
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const res = await newRequest.get(`/appointment/doctor/${doctorId}`);
+        const fetchedAppointments = res.data.appointments;
+
+        console.log('data', res.data)
+        console.log('appointments: ', fetchedAppointments)
+
+        if (fetchedAppointments.length == 0) setIsEmpty(true);
+
+        setAppointments(fetchedAppointments);
+      }catch (error) {
+        console.log('Error fetching appointments: ', error);
+      }
+    }
+
+    fetchAppointments();
+  }, [])
+
   return (
     <div className="appointment-list">
       <ul>
-        {appointments.map((appointment) => (
+        {/* {appointments.map((appointment) => (
           <li key={appointment.id}>
             <span>{appointment.patient}</span>
             <span>{appointment.date}</span>
             <span>{appointment.time}</span>
           </li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
