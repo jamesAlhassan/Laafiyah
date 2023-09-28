0303feb67381b4ec020f204594f49f4d1f0210ba
James Alhassan <jamesalhassan052@gmail.com>
2023-09-28 06:10:19 +0000

    Update

--- client/src/pages/Dashboard/Profile.js
+++ client/src/pages/Dashboard/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 // import "./style.css";
-
+import uploadImage from "../../assets/uploadImage.png";
 function ImageUpload() {
   const [image, setImage] = useState(null);
   const hiddenFileInput = useRef(null);
@@ -71,41 +71,124 @@ function ImageUpload() {
   };
 
   return (
-    <div className='image-upload-container'>
-      <div className='box-decoration'>
-        <label htmlFor='image-upload-input' className='image-upload-label'>
-          {image ? image.name : "Choose an image"}
-        </label>
-        <div onClick={handleClick} style={{ cursor: "pointer" }}>
-          {image ? (
-            <img
-              src={URL.createObjectURL(image)}
-              alt='upload image'
-              className='img-display-after'
-            />
-          ) : (
-            <img
-              src='./photo.png'
-              alt='upload image'
-              className='img-display-before'
-            />
-          )}
+    <div>
+      <div class='wrapper'>
+        <div class='pro'>
+          <div class='content'>
+            <h1>Edit Profile</h1>
+            <form onSubmit={handleUploadButtonClick}>
+              {/* <!-- Photo --> */}
+              <fieldset>
+                {/* <div class='grid-35'>
+                  <label for='avatar'>Your Photo</label>
+                </div>
+                <div class='grid-65'>
+                  <span class='photo' title='Upload your Avatar!'></span>
+                  <input
+                    type='file'
+                    class='btn'
+                    name='file'
+                    onChange={handleFile}
+                  />
+                </div> */}
 
-          <input
-            id='image-upload-input'
-            type='file'
-            onChange={handleImageChange}
-            ref={hiddenFileInput}
-            style={{ display: "none" }}
-          />
+                <div className='image-upload-container'>
+                  <div className='box-decoration'>
+                    <label
+                      htmlFor='image-upload-input'
+                      className='image-upload-label'
+                    >
+                      {image ? image.name : "Choose an image"}
+                    </label>
+                    <div onClick={handleClick} style={{ cursor: "pointer" }}>
+                      {image ? (
+                        <img
+                          src={URL.createObjectURL(image)}
+                          alt='upload image'
+                          className='img-display-after'
+                        />
+                      ) : (
+                        <img
+                          src={uploadImage}
+                          alt='upload image'
+                          className='img-display-before'
+                        />
+                      )}
+
+                      <input
+                        id='image-upload-input'
+                        type='file'
+                        onChange={handleImageChange}
+                        ref={hiddenFileInput}
+                        style={{ display: "none" }}
+                      />
+                    </div>
+
+                    {/* <button
+                      className='image-upload-button'
+                      onClick={handleUploadButtonClick}
+                    >
+                      Upload
+                    </button> */}
+                  </div>
+                </div>
+              </fieldset>
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='fname'>First Name</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='fname' tabindex='1' />
+                </div>
+              </fieldset>
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='lname'>Last Name</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='lname' tabindex='2' />
+                </div>
+              </fieldset>
+
+              {/* <!-- Location --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='location'>Location</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='location' tabindex='4' />
+                </div>
+              </fieldset>
+              {/* <!-- Country --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='country'>Country</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='text' id='country' tabindex='5' />
+                </div>
+              </fieldset>
+              {/* <!-- Email --> */}
+              <fieldset>
+                <div class='grid-35'>
+                  <label for='email'>Email Address</label>
+                </div>
+                <div class='grid-65'>
+                  <input type='email' id='email' tabindex='6' />
+                </div>
+              </fieldset>
+
+              <fieldset>
+                <input type='button' class='Btn cancel' value='Cancel' />
+                <input
+                  type='submit'
+                  className='Btn image-upload-button'
+                  value='Save Changes'
+                />
+              </fieldset>
+            </form>
+          </div>
         </div>
-
-        <button
-          className='image-upload-button'
-          onClick={handleUploadButtonClick}
-        >
-          Upload
-        </button>
       </div>
     </div>
   );
