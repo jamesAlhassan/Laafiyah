51492f2c60914b78d549caf99ec7c38fa9073be6
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-30 22:00:11 +0000

    Implement confirm booking modal in Confrim page

--- client/src/pages/booking/ConfirmBooking.js
+++ client/src/pages/booking/ConfirmBooking.js
@@ -1,19 +1,19 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import DoctorSummary from "../../components/doctorSummary/DoctorSummary";
 import './ConfirmBooking.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import newRequest from "../../utils/newRequest";
-import ConfirmationPopup from "../../components/popups/ConfirmationPopup";
 import ConfirmBookingModal from "../../components/modals/ConfirmBookingModal";
 
 const ConfirmBooking = () => {
 
     const [showModal, setShowModal] = useState(false);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    const [isInputDisabled, setIsInputDisabled] = useState(false);
     const [phone, setPhone] = useState('');
     const [notes, setNotes] = useState('');
     const location = useLocation();
     const { appointment, doctor } = location.state || {};
-    const [showPopup, setShowPopup] = useState(false);
 
     const navigate = useNavigate();
 
@@ -33,6 +33,9 @@ const ConfirmBooking = () => {
             await newRequest.post('/appointment', { ...finalAppointment })
                 .then((res) => {
                     console.log('Appointment added successfully');
+                    setIsButtonDisabled(true);
+                    setIsInputDisabled(true);
+                    setShowModal(true);
                 })
                 .catch((error) => {
                     console.log(error);
@@ -40,86 +43,70 @@ const ConfirmBooking = () => {
         } catch (error) {
             console.log(error);
         }
-
-        // setShowPopup(true);
-        // Show the modal
-        setShowModal(true);
     }
 
-    const closeModal = () => {
-        // Close the modal
+    const handleCloseModal = () => {
         setShowModal(false);
-
-        // Redirect to the dashboard (you can use react-router-dom for this)
-        // Example: history.push('/dashboard');
-        navigate('/dasbhboard');
+        // Close the modal and redirect to the dashboard
+        navigate('/');
     };
 
-    const closePopup = () => {
-        // close the popup and redirect to dashboard
-        setShowPopup(false);
-        navigate('/dasbhboard');
-    }
-
     return (
-        <>
-            {showPopup && (
-                <ConfirmationPopup onClose={closePopup} onConfirm={() => navigate('/dashboard')} />
-            )}
-            <div className="confirm-booking">
-                <div className="confirm-booking-container">
-                    <div className="booking-details">
-                        <li><h4>In Clinic Appointment</h4></li>
-                        <li>
-                            <p>On {appointment.day} <br />
-                                Change Date & Time</p>
-                            <p>at {appointment.time}</p>
-                        </li>
-                        <li>
-                            <DoctorSummary key={doctor._id} doctor={doctor} />
-                        </li>
-                    </div>
-
-                    <div className="enter-phone">
-
-                        <form onSubmit={(e) => e.preventDefault()}>
-                            <h4>Enter phone Number</h4>
-                            {/* Phone Number */}
-                            <label htmlFor="phone">Phone number</label>
-                            <input
-                                type="text"
-                                id="phone"
-                                name="phone"
-                                value={phone}
-                                placeholder="enter phone number"
-                                onChange={(e) => setPhone(e.target.value)}
-                                required
-                            />
-                            {/* Addtional info */}
-                            <label htmlFor="notes">Add any addtional information</label>
-                            <textarea
-                                type="text"
-                                id="notes"
-                                name="notes"
-                                value={notes}
-                                rows="8"
-                                cols="50"
-                                placeholder='Add any addtional Info'
-                                onChange={(e) => setNotes(e.target.value)}>
-                            </textarea><br />
-                            <button type="button" onClick={handleContinue}>
-                                Continue
-                            </button>
-                        </form>
-                    </div>
+        <div className="confirm-booking">
+            <div className="confirm-booking-container">
+                <div className="booking-details">
+                    <li><h4>In Clinic Appointment</h4></li>
+                    <li>
+                        <p>On {appointment.day} <br />
+                            Change Date & Time</p>
+                        <p>at {appointment.time}</p>
+                    </li>
+                    <li>
+                        <DoctorSummary key={doctor._id} doctor={doctor} />
+                    </li>
                 </div>
+
+                <div className="enter-phone">
+
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <h4>Enter phone Number</h4>
+                        {/* Phone Number */}
+                        <label htmlFor="phone">Phone number</label>
+                        <input
+                            type="text"
+                            id="phone"
+                            name="phone"
+                            disabled={isInputDisabled}
+                            value={phone}
+                            placeholder="enter phone number"
+                            onChange={(e) => setPhone(e.target.value)}
+                            required
+                        />
+                        {/* Addtional info */}
+                        <label htmlFor="notes">Add any addtional information</label>
+                        <textarea
+                            type="text"
+                            id="notes"
+                            name="notes"
+                            value={notes}
+                            rows="8"
+                            cols="50"
+                            placeholder='Add any addtional Info'
+                            onChange={(e) => setNotes(e.target.value)}>
+                        </textarea><br />
+                        <button type="button" disabled={isButtonDisabled}
+                            onClick={handleContinue}>
+                            Continue
+                        </button>
+                    </form>
+                </div>
+                {/* Render the confirmation modal */}
+                {showModal && (
+                    <ConfirmBookingModal
+                        onClose={handleCloseModal} />
+                )}
             </div>
-            <ConfirmBookingModal
-                isOpen={showModal}
-                onRequestClose={closeModal}
-                onConfirm={() => { /* Handle onConfirm logic */ }}
-            />
-        </>
+        </div>
     )
 }
 
