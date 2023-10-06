0e90c81717ae230c1aef583a229be813c803046e
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-10-06 10:58:12 +0000

    Handle bug in displaying reviews

--- client/src/components/review/Review.js
+++ client/src/components/review/Review.js
@@ -5,12 +5,12 @@ import RatingStars from '../rating/Rating';
 const Review = ({ review }) => {
     return (
         <div className='review'>
-            <h4>{review.title}</h4>
-            <RatingStars rating={review.rating} />
-            <p>{review.content}</p>
+            <h4>{review?.title}</h4>
+            <RatingStars rating={review?.rating} />
+            <p>{review?.content}</p>
             <div className='subscript'>
-                <p>{formatDate(review.createdAt)}</p>
-                <p>{review.patient.firstName} {review.patient.lastName}</p>
+                <p>{formatDate(review?.createdAt)}</p>
+                <p>{review?.patient.firstName} {review?.patient.lastName}</p>
             </div>
         </div>
     );
